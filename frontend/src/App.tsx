cefae2e74191372bb90ae8d3c6375be984f271da
murky(길무성) <lotstar87@gmail.com>
2021-11-11 16:20:35 +0900

    feat: 연습방 추가 (개발 테스트용) 및 그림판, 펜 WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { store } from './store';
 import Home from 'pages/Home';
 import Room from 'pages/Room';
+import PracticeRoom from 'pages/PracticeRoom';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/room/:roomId" element={<Room />} />
+          <Route path="/practice-room" element={<PracticeRoom />} />
         </Routes>
       </ChakraProvider>
     </Provider>
