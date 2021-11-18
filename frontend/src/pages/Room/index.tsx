d16721b1d5003b51c2d49091c57af2555dd2a73b
code-sign <snow6348@gmail.com>
2021-11-19 00:19:16 +0900

    style: 공백제거

--- frontend/src/pages/Room/index.tsx
+++ frontend/src/pages/Room/index.tsx
@@ -17,24 +17,23 @@ const Room = () => {
     <PageTemplate id="room">
       <Container p={0} maxW="100%">
         <Grid h="100vh" templateRows="repeat(8, 1fr)" templateColumns="repeat(8, 1fr)" gap={2}>
-
           <GridItem rowSpan={1} colSpan={8} bg="tomato">
             <Heading mt={10} mb={10} textAlign="center" fontSize={40} color="#000">
               ROOM
             </Heading>
           </GridItem>
 
-          <GridItem rowSpan={{ base: 1,  xl: 7 }} colSpan={{ base: 8, xl: 2 }} bg="gray.600">
+          <GridItem rowSpan={{ base: 1, xl: 7 }} colSpan={{ base: 8, xl: 2 }} bg="gray.600">
             참가자 목록
           </GridItem>
 
-          <GridItem rowSpan={{base: 4, md: 7}} colSpan={{ base: 8,  md: 5, xl: 4 }} bg="white">
+          <GridItem rowSpan={{ base: 4, md: 7 }} colSpan={{ base: 8, md: 5, xl: 4 }} bg="white">
             <Text fontSize="6xl" color="#000">
               drawing~
             </Text>
           </GridItem>
 
-          <GridItem rowSpan={{base: 2, md: 7}} colSpan={{ base: 8, md: 3, xl: 2 }} bg="teal">
+          <GridItem rowSpan={{ base: 2, md: 7 }} colSpan={{ base: 8, md: 3, xl: 2 }} bg="teal">
             채팅
           </GridItem>
         </Grid>
