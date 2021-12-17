5ffe525f040a5c87053f98ab1497bef5efd0dc53
code-sign <snow6348@gmail.com>
2021-12-17 14:59:18 +0900

    fix: 상수 네이밍 변경

--- frontend/src/pages/Home/components/CameraControl/index.jsx
+++ frontend/src/pages/Home/components/CameraControl/index.jsx
@@ -2,13 +2,14 @@ import * as THREE from 'three';
 import { useEffect, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 
+const VEC = new THREE.Vector3();
+const STEP_VALUE = 0.06;
+
 const CameraControl = () => {
-  const vec = new THREE.Vector3();
-  const step = 0.06;
   const [isAction, setIsAction] = useState(true);
 
   useFrame((state) => {
-    isAction && state.camera.position.lerp(vec.set(0, 0, 12), step);
+    isAction && state.camera.position.lerp(VEC.set(0, 0, 12), STEP_VALUE);
   });
 
   useEffect(() => {
