7d679019ab14b14d3dda3788fa1319da5c1c5b01
codeJS <dodortus@gmail.com>
2021-12-19 17:44:53 +0900

    feat: 룸 레이아웃 배치 변경

--- frontend/src/pages/Room/containers/ChatContainer.tsx
+++ frontend/src/pages/Room/containers/ChatContainer.tsx
@@ -2,17 +2,15 @@ import { useCallback, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Dispatch, select } from 'store';
 
-import { Flex, Box, Textarea, Button } from '@chakra-ui/react';
-import { sendMessage, SocketMessageType } from 'modules/socket';
+import { Flex, Box } from '@chakra-ui/react';
 import event from 'modules/event';
 import cn from 'classnames';
 
 const ChatContainer = () => {
-  const { userInfo, participants } = useSelector(select.room.state);
+  const { participants } = useSelector(select.room.state);
   const { chatList } = useSelector(select.chat.state);
   const dispatch = useDispatch<Dispatch>();
   const listWrapRef = useRef<HTMLDivElement>(null);
-  const textRef = useRef<HTMLTextAreaElement>(null);
 
   const handleScrollToBottom = useCallback(() => {
     const $listWrap = listWrapRef.current;
@@ -22,53 +20,6 @@ const ChatContainer = () => {
 
     $listWrap.scrollTop = $listWrap.scrollHeight;
   }, [listWrapRef]);
-
-  const handleSendMessage = useCallback(() => {
-    const message = textRef.current?.value;
-
-    if (!textRef.current || !message) {
-      return;
-    }
-
-    dispatch.chat.addChat({
-      isMine: true,
-      nickName: userInfo?.nickName,
-      message,
-    });
-
-    sendMessage({
-      type: SocketMessageType.Chat,
-      body: message,
-    });
-
-    textRef.current.value = '';
-    setTimeout(() => handleScrollToBottom(), 0);
-  }, [dispatch, userInfo, handleScrollToBottom]);
-
-  const handleKeyUp = useCallback(
-    (e) => {
-      if (e.shiftKey) {
-        return;
-      }
-
-      if (e.key === 'Enter') {
-        e.preventDefault();
-        handleSendMessage();
-      }
-    },
-    [handleSendMessage]
-  );
-
-  // NOTE: 한글 마지막 글자 두번 처리되는이슈로 keydown에서도 이벤트 핸들링 필요
-  const handleKeyDown = useCallback((e) => {
-    if (e.shiftKey) {
-      return;
-    }
-
-    if (e.key === 'Enter') {
-      e.preventDefault();
-    }
-  }, []);
 
   const findParticipantInfo = useCallback(
     (userId: string) => {
@@ -109,21 +60,6 @@ const ChatContainer = () => {
               </li>
             ))}
           </ul>
-        </Box>
-        <Box className="chat-input-wrap" bg="rgba(0,0,0,0.1)" padding="5px">
-          <Flex h="100%" justifyContent="space-between">
-            <Textarea
-              placeholder="메시지를 입력해주세요!"
-              background="#fff"
-              resize="none"
-              onKeyDown={handleKeyDown}
-              onKeyUp={handleKeyUp}
-              ref={textRef}
-            />
-            <Button w="72px" h="100%" colorScheme="yellow" variant="solid" color="#fff" onClick={handleSendMessage}>
-              Send
-            </Button>
-          </Flex>
         </Box>
       </Flex>
     </Box>
--- frontend/src/pages/Room/containers/UserListContainer.tsx
+++ frontend/src/pages/Room/containers/UserListContainer.tsx
@@ -1,31 +1,61 @@
-import { useSelector } from 'react-redux';
-import { select } from 'store';
-import { Flex, Box, Avatar, Badge, Text } from '@chakra-ui/react';
+import { useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Dispatch, select } from 'store';
 
-const UserListContainer = () => {
+import { Flex, Box, Avatar, Badge, Text } from '@chakra-ui/react';
+import event from 'modules/event';
+
+interface Props {
+  position: 'left' | 'right';
+}
+
+const UserListContainer = ({ position = 'left' }: Props) => {
   const { participants } = useSelector(select.room.state);
+  const dispatch = useDispatch<Dispatch>();
+
+  const findParticipantInfo = useCallback(
+    (userId: string) => {
+      return participants.find((data) => data.userId === userId);
+    },
+    [participants]
+  );
+
+  const onChat = useCallback(
+    ({ senderId, body }) => {
+      const nickName = findParticipantInfo(senderId)?.nickName ?? null;
+      if (nickName) {
+        dispatch.chat.addChat({
+          nickName,
+          message: body,
+        });
+      }
+    },
+    [dispatch, findParticipantInfo]
+  );
+
+  useEffect(() => {
+    event.removeAllListeners('chat');
+    event.on('chat', onChat);
+  }, [onChat]);
 
   return (
-    <div>
-      <Text>참여자목록 ({participants.length})</Text>
-      <ul>
-        {participants.map(({ nickName }) => {
-          return (
-            <li>
-              <Flex bg="white" margin={1} padding={2}>
-                <Avatar src="https://bit.ly/sage-adebayo" />
-                <Box ml="3">
-                  <Text fontWeight="bold">{nickName}</Text>
-                  <Badge ml="1" colorScheme="green">
-                    점수: 0
-                  </Badge>
-                </Box>
-              </Flex>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ul className={`user-list ${position}`}>
+      {participants.map(({ nickName }) => {
+        return (
+          <li>
+            <Flex bg="white" margin={1} padding={2}>
+              <Avatar src="https://bit.ly/sage-adebayo" />
+              <Box ml="3">
+                <Text fontWeight="bold">{nickName}</Text>
+                <Badge ml="1" colorScheme="green">
+                  점수: 0
+                </Badge>
+              </Box>
+            </Flex>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
