f0d0ebf286608056e0e9b16f7b01b96d59a4c174
code-sign <snow6348@gmail.com>
2022-04-14 17:57:13 +0900

    feat: 타이머 ui 추가

--- frontend/src/pages/Room/components/GameTimer/index.tsx
+++ frontend/src/pages/Room/components/GameTimer/index.tsx
@@ -1,10 +1,37 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
+import { useEffect, useMemo, useState } from 'react';
+import { format } from 'date-fns';
+
 import './index.scss';
 
+const LIMIT_TIME = 90;
+
 const GameTimer = () => {
+  const [timeCount, setTimeCount] = useState(LIMIT_TIME);
+  const timeFormat = useMemo(() => format(new Date(2022, 1, 1, 1, 0, timeCount), 'mm:ss'), [timeCount]);
+
+  useEffect(() => {
+    if (timeCount === 0) return;
+    const timer = setInterval(() => setTimeCount((timeCount) => timeCount - 1), 1000);
+    return () => clearInterval(timer);
+  }, [timeCount]);
+
+  // 10초 미만시 강조 컬러
+  const isNeedSpeedUp = timeCount < 10;
+  const isTimeout = timeCount === 0;
+
   return (
-    <Box w="80px" h="80px" p={5} color="black" bg="white" border="solid 4px green" borderRadius={30}>
-      99
+    <Box className="game-timer">
+      <Text className="timer-emoji">⏰</Text>
+      {isTimeout ? (
+        <Text fontSize="sm" color="gray.500">
+          시간초과!
+        </Text>
+      ) : (
+        <Text fontSize="xl" color={isNeedSpeedUp ? 'red.500' : 'blackAlpha.900'}>
+          {timeFormat}
+        </Text>
+      )}
     </Box>
   );
 };
