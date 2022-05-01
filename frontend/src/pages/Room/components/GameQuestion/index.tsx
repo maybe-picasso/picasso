76d1a7bfb99d383270d9aafa132215de823fc128
codeJS <dodortus@gmail.com>
2022-05-01 15:40:24 +0900

    chore: 퀴즈 단어 영역 요소 추가

---
+++ frontend/src/pages/Room/components/GameQuestion/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const GameQuestion = () => {
+  return (
+    <Box bg="green.400" minWidth="100px" textAlign="center" fontWeight={600} p={2} color="white" borderRadius="6">
+      스타벅스
+    </Box>
+  );
+};
+
+export default GameQuestion;
