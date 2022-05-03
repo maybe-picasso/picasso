a92d4450bb5a9e3149ab462de5882968d2f6d0db
code-sign <snow6348@gmail.com>
2022-05-03 23:51:45 +0900

    fix: 등장 모션 스텝 빠르게 수정

--- frontend/src/pages/Home/components/CameraControl/index.jsx
+++ frontend/src/pages/Home/components/CameraControl/index.jsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 
 const VEC = new THREE.Vector3();
-const STEP_VALUE = 0.06;
+const STEP_VALUE = 0.2;
 
 const CameraControl = () => {
   const [isAction, setIsAction] = useState(true);
 
   useFrame((state) => {
-    isAction && state.camera.position.lerp(VEC.set(0, 0, 12), STEP_VALUE);
+    isAction && state.camera.position.lerp(VEC.set(0, 0, 15), STEP_VALUE);
   });
 
   useEffect(() => {
