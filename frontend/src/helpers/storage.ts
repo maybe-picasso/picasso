d36d5062328642cdf6b94cf12fa074de5f2e3c38
codeJS <dodortus@gmail.com>
2022-06-10 10:40:47 +0900

    feat: 로컬스토리지 헬퍼 추가

---
+++ frontend/src/helpers/storage.ts
@@ -0,0 +1,37 @@
+/**
+ * 로컬스토리지 쓰기
+ * @param key
+ * @param value
+ */
+export function setStorage(key: string, value: any): void {
+  value = JSON.stringify(value);
+  localStorage[key] = value;
+}
+
+/**
+ * 로컬스토리지 읽기
+ * @param key
+ */
+export function getStorage(key: string): any {
+  let value = localStorage[key];
+  if (!value) {
+    // console.info(`localStorage에 ${key}는 없습니다.`);
+    return null;
+  }
+
+  try {
+    value = JSON.parse(value);
+  } catch (error) {
+    console.error('getStorage catch(error)', error);
+  }
+
+  return value;
+}
+
+/**
+ * 로컬스토리지 특정 key 데이터 삭제
+ * @param key
+ */
+export function deleteStorage(key: string): void {
+  localStorage.removeItem(key);
+}
