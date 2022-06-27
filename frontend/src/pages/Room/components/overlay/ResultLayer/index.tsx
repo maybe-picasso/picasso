1c8bdbc1f80d9aa6021de29c222fd38be42d182a
codeJS <dodortus@gmail.com>
2022-06-27 23:11:58 +0900

    chore: ResultLayer minHeight 적용

--- frontend/src/pages/Room/components/overlay/ResultLayer/index.tsx
+++ frontend/src/pages/Room/components/overlay/ResultLayer/index.tsx
@@ -8,18 +8,19 @@ interface Props {
   title: string | React.ReactNode;
 }
 
+const variants = {
+  initial: { opacity: 0, y: 30, scale: 0.85 },
+  visible: { opacity: 1, y: 0, scale: 1 },
+};
+
 const ResultLayer = ({ children, title }: Props) => {
-  const variants = {
-    initial: { opacity: 0, y: 30, scale: 0.85 },
-    visible: { opacity: 1, y: 0, scale: 1 },
-  };
   return (
     <motion.div initial="initial" animate="visible" variants={variants} transition={{ duration: 0.3 }}>
       <Heading mb={5} color="white" textAlign="center">
         {title}
       </Heading>
       {children && (
-        <Box bgColor="white" width="370px" borderRadius={6} padding={6}>
+        <Box bgColor="white" width={370} minHeight={200} borderRadius={6} padding={6} marginX="auto">
           {children}
         </Box>
       )}
