285136cf0e0902d44b58b573c467538427dec875
codeJS <dodortus@gmail.com>
2022-07-26 01:09:47 +0900

    feat: 어바웃 페이지 추가

---
+++ frontend/src/pages/About/index.tsx
@@ -0,0 +1,43 @@
+import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@chakra-ui/react';
+import { Canvas } from '@react-three/fiber';
+import { TrackballControls } from '@react-three/drei'; //7.0.1 써 에러x
+
+import Typo3D from './components/Typo3D';
+import Image3D from './components/Image3D';
+import Earth from './components/Earth';
+import CameraControl from './components/CameraControl';
+
+import './index.scss';
+
+const About = () => {
+  return (
+    <div className="about-wrap">
+      <Canvas camera={{ position: [20, 20, 300], fov: 30 }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+
+        <Suspense fallback={null}>
+          <Image3D size={1.2} position={[0, 0.1, 0]} />
+          <mesh position={[0, 1.8, 0]}>
+            <Typo3D hAlign="left" color={'#0e978c'} size={1.4} children="Picasso" />
+          </mesh>
+
+          <Earth radius={2} position={[0, -3.4, 0]} />
+          <CameraControl />
+        </Suspense>
+
+        <TrackballControls panSpeed={0.4} minDistance={9} maxDistance={40} />
+      </Canvas>
+
+      <Link to="/room/abc">
+        <Button w="250px" h="60px" colorScheme="yellow" size="lg" fontWeight="bold" variant="solid">
+          Github
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default About;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import Room from 'pages/Room';
+import About from 'pages/About';
 import PATHS from './paths';
 
 const AppRoutes = () => {
@@ -8,6 +9,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path={PATHS.HOME} element={<Home />} />
       <Route path={PATHS.ROOM} element={<Room />} />
+      <Route path={PATHS.ABOUT} element={<About />} />
     </Routes>
   );
 };
