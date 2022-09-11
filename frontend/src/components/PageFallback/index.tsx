4f309577045c21e0565d03dffc56e5cf8d2da3ba
codeJS <dodortus@gmail.com>
2022-09-12 01:22:01 +0900

    chore: PageFallback 컬러수정

--- frontend/src/components/PageFallback/index.tsx
+++ frontend/src/components/PageFallback/index.tsx
@@ -4,7 +4,7 @@ import './index.scss';
 const PageFallback = () => {
   return (
     <Center className="page-fallback">
-      <Spinner thickness="4px" emptyColor="gray.200" color="purple.500" size="xl" />
+      <Spinner thickness="4px" emptyColor="gray.200" color="gray.600" size="xl" />
     </Center>
   );
 };
