633497b4094c4afef4ec21b848b11e3d4ec889a7
codeJS <dodortus@gmail.com>
2022-11-25 23:44:07 +0900

    feat: AppController 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { SocketGateway } from './app.socket';
 import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [UsersModule, MoviesModule],
+  controllers: [AppController],
   providers: [SocketGateway],
 })
 export class AppModule {}
