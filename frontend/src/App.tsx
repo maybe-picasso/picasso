01d74cd567d66fe2db23adb719054b794a1a9a8e
codeJS <dodortus@gmail.com>
2022-12-28 00:23:46 +0900

    feat: react-query 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,33 @@
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChakraProvider } from '@chakra-ui/react';
 import { store } from './store';
 import { theme } from './theme';
 import AppRoutes from 'routes';
 import ErrorBoundary, { ErrorFallback, errorHandler } from 'components/ErrorBoundary';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
+
 const App = () => {
   return (
     <Provider store={store}>
       <ChakraProvider theme={theme}>
-        <ErrorBoundary FallbackComponent={ErrorFallback} onError={errorHandler}>
-          <AppRoutes />
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onError={errorHandler}
+          onReset={() => queryClient.resetQueries()}
+        >
+          <QueryClientProvider client={queryClient}>
+            <AppRoutes />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </ErrorBoundary>
       </ChakraProvider>
     </Provider>
