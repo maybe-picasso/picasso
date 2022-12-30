81342a558d2d560a1c80c3e91a1e089bdcf788f4
codeJS <dodortus@gmail.com>
2022-12-30 21:54:53 +0900

    chore: 로그인 영역 스타일 수정

--- frontend/src/pages/Home/containers/LoginProfileContainer/index.tsx
+++ frontend/src/pages/Home/containers/LoginProfileContainer/index.tsx
@@ -1,12 +1,12 @@
 import { BsGoogle } from 'react-icons/bs';
 
-import { Avatar, Spinner } from '@chakra-ui/react';
+import { Avatar } from '@chakra-ui/react';
 import { LOCAL_STORAGE, deleteStorage } from 'helpers/storage';
 import { useUserInfoQuery } from 'queries';
 import './index.scss';
 
 const LoginProfileContainer = () => {
-  const { isLoading, data: userInfo } = useUserInfoQuery();
+  const { data: userInfo } = useUserInfoQuery();
 
   const handleLogin = () => {
     window.location.href = 'http://localhost:3000/auth/google';
@@ -19,9 +19,7 @@ const LoginProfileContainer = () => {
 
   return (
     <div className="login-profile">
-      {isLoading ? (
-        <Spinner />
-      ) : userInfo ? (
+      {userInfo ? (
         <button type="button" onClick={handleLogout} title={userInfo.displayName}>
           <p>{userInfo.displayName}</p>
           <span className="circle">
