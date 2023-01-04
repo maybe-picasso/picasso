6f450cdbcf69a6d15d4dbf62e24a34237ced00e7
codeJS <dodortus@gmail.com>
2023-01-05 00:15:00 +0900

    chore: 테스트 파일 제거

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,9 @@ import { AppController } from './app.controller';
 import { SocketGateway } from './app.socket';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/picasso'), AuthModule, UsersModule, CatsModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/picasso'), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [SocketGateway],
 })
