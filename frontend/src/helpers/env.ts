7f9b4390a49f60983fcee70f3af266820cc47068
codeJS <dodortus@gmail.com>
2023-01-19 22:08:36 +0900

    chore: env 수정

--- frontend/src/helpers/env.ts
+++ frontend/src/helpers/env.ts
@@ -3,4 +3,4 @@ import { homepage } from '../../package.json';
 export const isNodeProdcution: boolean = process.env.NODE_ENV === 'production';
 export const isStorybook: boolean = process.env.STORYBOOK === 'true';
 export const basename = isNodeProdcution ? homepage : '';
-export const apiServer = isNodeProdcution ? 'http://localhost:3000' : 'http://localhost:3000';
+export const apiServer = isNodeProdcution ? 'https://maybe-picasso.herokuapp.com' : 'http://localhost:3000';
